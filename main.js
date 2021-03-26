099ad07599d0e729a15159d2df6122cf41feef38
Patrick Duvall <duvallp208@gmail.com>
2021-03-26 13:40:34 -0600

    Add message when all affirmations seen

--- main.js
+++ main.js
@@ -1,58 +1,77 @@
 
 var mantras = [
-  'Breathing in, I send myself love.Breathing out, I send love to someone else who needs it.',
-  'Don’t let yesterday take up too much of today.',
-  'Every day is a second chance.',
-  'Tell the truth and love everyone.',
-  'I am free from sadness.',
-  'I am enough.',
-  'In the beginning it is you, in the middle it is you and in the end it is you.',
-  'I love myself.',
-  'I am present now.',
-  'Inhale the future, exhale the past.',
-  'This too shall pass.',
-  'Yesterday is not today.',
-  'The only constant is change.',
-  'Onward and upward.',
-  'I am the sky, the rest is weather.'
+  'Breathing in, I send myself love.Breathing out, I send love to someone else who needs it.'
+  // 'Don’t let yesterday take up too much of today.',
+  // 'Every day is a second chance.',
+  // 'Tell the truth and love everyone.',
+  // 'I am free from sadness.',
+  // 'I am enough.',
+  // 'In the beginning it is you, in the middle it is you and in the end it is you.',
+  // 'I love myself.',
+  // 'I am present now.',
+  // 'Inhale the future, exhale the past.',
+  // 'This too shall pass.',
+  // 'Yesterday is not today.',
+  // 'The only constant is change.',
+  // 'Onward and upward.',
+  // 'I am the sky, the rest is weather.'
 ]
 
 var affirmations = [
   'I forgive myself and set myself free.',
-  'I believe I can be all that I want to be.',
-  'I am in the process of becoming the best version of myself.',
-  'I have the freedom & power to create the life I desire.',
-  'I choose to be kind to myself and love myself unconditionally.',
-  'My possibilities are endless.',
-  'I am worthy of my dreams.',
-  'I am enough.',
-  'I deserve to be healthy and feel good.',
-  'I am full of energy and vitality and my mind is calm and peaceful.',
-  'Every day I am getting healthier and stronger.',
-  'I honor my body by trusting the signals that it sends me.',
-  'I manifest perfect health by making smart choices.',
+  'I believe I can be all that I want to be.'
+  // 'I am in the process of becoming the best version of myself.',
+  // 'I have the freedom & power to create the life I desire.',
+  // 'I choose to be kind to myself and love myself unconditionally.',
+  // 'My possibilities are endless.',
+  // 'I am worthy of my dreams.',
+  // 'I am enough.',
+  // 'I deserve to be healthy and feel good.',
+  // 'I am full of energy and vitality and my mind is calm and peaceful.',
+  // 'Every day I am getting healthier and stronger.',
+  // 'I honor my body by trusting the signals that it sends me.',
+  // 'I manifest perfect health by making smart choices.',
 ]
+
+var remainingAffirmations = affirmations
 
 
 var displayedMessage = document.querySelector('.displayed-message')
+var seenAllMessage = document.querySelector('.seen-all-message')
 var messageButton = document.querySelector('.receive-message')
 var bell = document.querySelector('.bell')
 var mantraRadio = document.querySelector('#mantra-radio')
 var affirmationRadio = document.querySelector('#affirmation-radio')
+var messageDelivery = document.querySelector('.message-delivery')
 
-messageButton.addEventListener('click', displayAffirmation)
+messageButton.addEventListener('click', displayMessage)
 
-function displayAffirmation() {
+function displayMessage() {
   bell.classList.add('hidden')
   displayedMessage.classList.remove('hidden')
   if( mantraRadio.checked ) {
     displayedMessage.innerText = randomElement(mantras)
   } else if(affirmationRadio.checked) {
-    displayedMessage.innerText = randomElement(affirmations)
+    affirmation = randomElement(remainingAffirmations)
+    if (remainingAffirmations.length === affirmations.length){
+      messageDelivery.setAttribute('class', 'message-delivery')
+      seenAllMessage.classList.add('hidden')
+    }
+    remainingAffirmations = remainingAffirmations.filter(aff => aff !== affirmation )
+    displayedMessage.innerText = affirmation
+    if(!remainingAffirmations.length){
+      seenAllMessage.classList.remove('hidden')
+      seenAllMessage.innerText= 'You have seen all available affirmations, view some mantras, ' +
+      'or continue receiving previously seen affirmations'
+      messageDelivery.setAttribute('class', 'message-delivery-seen')
+      remainingAffirmations = affirmations
+    }
   } else {
     displayedMessage.innerText = 'To not decide is still a decision'
   }
 }
+
+function 
 
 function randomElement(array) {
   return array[Math.floor(Math.random() * array.length)]
